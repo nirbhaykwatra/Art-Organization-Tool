72d828d05a399a8413b15c81d574cba2947c6436
Nirbhay Kwatra <contact@nirbhaykwatra.com>
2025-10-03 16:41:14 -0700

    Create ProcessAsset method and add data persistence to tool window

--- Editor/ArtAssetPreprocessor.cs
+++ Editor/ArtAssetPreprocessor.cs
@@ -5,21 +5,76 @@ using UnityEngine;
 
 public class ArtAssetPreprocessor : AssetPostprocessor
 {
-    private void OnPostprocessTexture(Texture2D texture)
+    private void ProcessAsset<T>(T asset)
     {
+        
         // If the model with the corresponding name exists in the project, move this texture into the Textures folder
         // of the model folder structure. If the Textures folder doesn't exist, create it (ModelName/Textures).
-        // Do the same with the model and materials as well
+        // Do the same with the model and materials as well.
+        
+        // TODO: Add naming convention support. When an asset is imported, check its name for prefixes defined in the tool.
+        //  If the asset doesn't have the defined prefix, assign it.
+        
+        // TODO: Add support for multiple meshes in the same folder. For example, if SM_handgun_01.fbx has variants named
+        //  SM_handgun_01_mag_full.fbx and SM_handgun_01_mag_empty.fbx, add them to the Meshes folder in the handgun_01 folder.
+        
+        // TODO: Come up with a way to allow the user to import models without automatic sorting, on a per case basis.
+        string artFolder = EditorPrefs.GetString("SCP.ArtToolSettings.ArtFolderPath", "");
+
+        if (artFolder == "")
+        {
+            Debug.LogError("ArtOrganizationTool: artFolder not set! Please set artFolder first!");
+            return;
+        }
+
+        if (!assetPath.Contains(artFolder))
+        {
+            Debug.LogError("Asset is not in the art folder!");
+            return;
+        }
+        
+        bool moveAssetOnImport = EditorPrefs.GetBool("SCP.ArtToolSettings.MoveAssetsOnImport", false);
+        if (!moveAssetOnImport) return;
+        
+        string assetName = Path.GetFileNameWithoutExtension(assetPath);
+        string assetExtension = Path.GetExtension(assetPath);
+        
+        string assetType = typeof(T).Name;
+        
+        Debug.Log($"Asset Path: {assetPath}");
+
+        switch (typeof(T).Name)
+        {
+            case "GameObject":
+                if (Directory.Exists(artFolder + "/" + assetName))
+                {
+                    File.Move(assetPath, artFolder + "/" + assetName + "/Meshes/" + assetName + assetExtension);
+                }
+                else
+                {
+                    AssetDatabase.CreateFolder(artFolder, assetName);
+                    AssetDatabase.CreateFolder(artFolder + "/" + assetName, "Meshes");
+                    File.Move(assetPath, artFolder + "/" + assetName + "/Meshes/" + assetName + assetExtension);
+                }
+                break;
+        }
+        
+    }
+    private void OnPostprocessTexture(Texture2D texture)
+    {
         string textureName = Path.GetFileNameWithoutExtension(assetPath);
     }
 
     private void OnPostprocessModel(GameObject model)
     {
+        string modelName = Path.GetFileNameWithoutExtension(assetPath);
         
+        ProcessAsset(model);
     }
 
     private void OnPostprocessMaterial(Material material)
     {
-        
+        string materialName = Path.GetFileNameWithoutExtension(assetPath);
     }
+
 }
--- Editor/ArtOrganizationWindow.cs
+++ Editor/ArtOrganizationWindow.cs
@@ -12,15 +12,37 @@ namespace ArtPipeline
         private static void OpenWindow() => GetWindow<ArtOrganizationWindow>();
         
         [ShowInInspector]
+        [Title("Create Folder Structure")]
         [AssetsOnly]
         [AssetSelector(Filter = "t:Model")]
         [InfoBox("Click the small three dots next to the selection field to see a list of model files in the project.")]
+        [PropertyOrder(-1)]
         private GameObject m_selectedObject;
 
         [ShowInInspector] 
         [FolderPath(RequireExistingPath = true)]
+        [PropertyOrder(-2)]
+        [Title("Basic Information")]
         private string m_artFolderPath;
         
+        [ShowInInspector]
+        [Title("Import Settings")]
+        [PropertyOrder(0)]
+        private bool m_moveAssetsOnImport;
+        
+        [ShowInInspector]
+        [PropertyOrder(2)]
+        private string m_meshPrefix;
+        
+        [ShowInInspector]
+        [PropertyOrder(2)]
+        private string m_texturePrefix;
+        
+        [ShowInInspector]
+        [PropertyOrder(2)]
+        private string m_materialPrefix;
+        
+        [PropertyOrder(-1)]
         [Button(ButtonSizes.Large)]
         private void CreateFolderStructure()
         {
@@ -31,6 +53,36 @@ namespace ArtPipeline
             AssetDatabase.CreateFolder(assetFolder, "Animations");
             AssetDatabase.CreateFolder(assetFolder, "Audio");
             AssetDatabase.Refresh();
+        }
+
+        [Button(ButtonSizes.Large)]
+        private void SaveSettings()
+        {
+            EditorPrefs.SetString("SCP.ArtToolSettings.ArtFolderPath", m_artFolderPath);
+            EditorPrefs.SetBool("SCP.ArtToolSettings.MoveAssetsOnImport", m_moveAssetsOnImport);
+            EditorPrefs.SetString("SCP.ArtToolSettings.MeshPrefix", m_meshPrefix);
+            EditorPrefs.SetString("SCP.ArtToolSettings.TexturePrefix", m_texturePrefix);
+            EditorPrefs.SetString("SCP.ArtToolSettings.MaterialPrefix", m_materialPrefix);
+            Debug.Log($"Set art organization preferences!");
+        }
+
+        protected override void OnEnable()
+        {
+            m_artFolderPath = EditorPrefs.GetString("SCP.ArtToolSettings.ArtFolderPath", "");
+            m_moveAssetsOnImport =  EditorPrefs.GetBool("SCP.ArtToolSettings.MoveAssetsOnImport", false);
+            m_meshPrefix = EditorPrefs.GetString("SCP.ArtToolSettings.MeshPrefix", "");
+            m_texturePrefix = EditorPrefs.GetString("SCP.ArtToolSettings.TexturePrefix", "");
+            m_materialPrefix = EditorPrefs.GetString("SCP.ArtToolSettings.MaterialPrefix", "");
+        }
+
+        protected override void OnBeginDrawEditors()
+        {
+            base.OnBeginDrawEditors();
+            EditorPrefs.SetString("SCP.ArtToolSettings.ArtFolderPath", m_artFolderPath);
+            EditorPrefs.SetBool("SCP.ArtToolSettings.MoveAssetsOnImport", m_moveAssetsOnImport);
+            EditorPrefs.SetString("SCP.ArtToolSettings.MeshPrefix", m_meshPrefix);
+            EditorPrefs.SetString("SCP.ArtToolSettings.TexturePrefix", m_texturePrefix);
+            EditorPrefs.SetString("SCP.ArtToolSettings.MaterialPrefix", m_materialPrefix);
         }
     }
 }
