77a23f25c707a9824ec9a992e9851f168773a26a
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-04-12 07:40:34 -0700

    Create basic JSON serialization service for storing settings

---
+++ Editor/Serialization.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace ArtPipeline
+{
+    public class Serializer
+    {
+        public void Serialize<T>(T data, string filePath)
+        {
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+        
+        public T Deserialize<T>(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<T>(json);
+        }
+    }
+}
